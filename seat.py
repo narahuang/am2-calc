2462e6e563e03322edaaaa9d9e85650b24c21d84
Nara Huang <narahuang@gmail.com>
2017-01-14 21:47:51 +0800

    First commit

---
+++ seat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Simple python script to calculate seat numbers 
+# Usage: seat.py seats economy business
+# Example: A KR-860 with 78% economy, 15% business and 7% first
+
+import sys
+
+def seat(seat, economy, business):
+    print("Economy: {0}".format(seat*economy/100))
+    print("Business: {0}".format(seat*business/100))
+    print("First: {0}".format(seat*(100-economy-business)/100))
+    
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {0} seats economy business".format(sys.argv[0]))
+    else:
+        seat(float(sys.argv[1]), float(sys.argv[2]), float(sys.argv[3]))
